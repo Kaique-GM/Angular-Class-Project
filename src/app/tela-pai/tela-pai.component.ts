11e04c92482041798f48a9cd5aa3d80e94920946
Kaique-GM <kaiquegmoficial@gmail.com>
2025-03-08 13:50:08 -0300

    Aula 3

---
+++ src/app/tela-pai/tela-pai.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-tela-pai',
+  imports: [RouterLink,RouterLinkActive,RouterOutlet],
+  templateUrl: './tela-pai.component.html',
+  styleUrl: './tela-pai.component.css'
+})
+export class TelaPaiComponent {
+
+}
